93fba697e259d38c22a7321ddbc847d7bd7bf529
Marcelo Ceccatto <58266548+mceccatto@users.noreply.github.com>
2023-06-03 18:13:31 -0300

    Correção de limite de upload.

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(logger('dev'));
 app.use(cors({
   origin: '*'
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
